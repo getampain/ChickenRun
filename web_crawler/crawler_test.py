eb5c689f92cb4730cf278752bf9487c6deaca900
swyoo <getampain@naver.com>
2021-05-03 23:51:52 +0900

    웹 크로울러 시작

---
+++ web_crawler/crawler_test.py
@@ -0,0 +1,24 @@
+import sys
+import platform
+import os
+
+
+"""
+ 가상환경 64비트 구축후 셀레니움 설치
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from bs4 import BeautifulSoup
+import time
+"""
+import pandas as pd
+from datetime import datetime
+
+
+print(1234)
+print(platform.architecture())
+
+
+print("PYTHONPATH:", os.environ.get('PYTHONPATH'))
+PYTHONPATH: None
+print("PATH:", os.environ.get('PATH'))
